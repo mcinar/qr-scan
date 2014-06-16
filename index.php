a665c90b0667d54ae662fb5138ee03957cda489f
Mehmet Cinar <passkontrol66@gmail.com>
2014-06-16 13:04:16 +0200

    art-works pages created

--- index.php
+++ index.php
@@ -11,12 +11,12 @@
 		<div class='app'>
 			<img src='assets/img/scan-logo.png'><br>
 			<img src='assets/img/qr-code.png' class='qr-code'><br>
-			<button class='connect'><a href='scan.html'>Connect</a></button><br>
+			<button class='connect'><a href='scan.php'>Connect</a></button><br>
 			<p class='c-info'>When you touch on the connect button your device will be connected to the camera.</p>
 			<hr>
 			<div class='u-info'>
 				<p>Do you want more information about the museum such as floor map, history of the musem etc.</p>
-				<p>Then <a href='info.html'>touch here</a> to go to the museum information page.</p>
+				<p>Then <a href='info.php'>touch here</a> to go to the museum information page.</p>
 			</div>
 		</div>
 	</body>
